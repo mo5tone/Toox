d9a50d6254115ec2c0ff7526361afade6d723f26
mo5tone <menjiefu@gmail.com>
2018-10-26 14:34:28 +0800

    project init.

---
+++ Toox/Toox.h
@@ -0,0 +1,19 @@
+//
+//  Toox.h
+//  Toox
+//
+//  Created by 门捷夫 on 2018/10/26.
+//  Copyright © 2018 门捷夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Toox.
+FOUNDATION_EXPORT double TooxVersionNumber;
+
+//! Project version string for Toox.
+FOUNDATION_EXPORT const unsigned char TooxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Toox/PublicHeader.h>
+
+
